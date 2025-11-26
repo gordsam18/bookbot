82e4a3fa0afe0e82cbdee3f0c2e58b644c860bfd
gordsam18 <gordsam18@gmail.com>
2025-11-25 21:44:28 -0700

    added sys args

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 from stats import (
     get_num_words,
     get_num_characters, 
     sort_dict
 )
+
 
 def main():
     get_report()
@@ -15,8 +17,11 @@ def get_book_text(path_to_file):
     return file_contents
 
 def get_report():
-    
-    book = get_book_text("books/frankenstein.txt")
+    if sys.argv =! 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book = get_book_text(sys.argv[1])
     num_words = get_num_words(book)
     num_characters = get_num_characters(book)
     chars_sorted_list = sort_dict(num_characters)
